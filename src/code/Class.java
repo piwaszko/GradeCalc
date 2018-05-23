966e8ff7fd1e1ef1b1cfa27592412725be15a96f
sayefiqbal <sayef20@gmail.com>
2018-05-22 21:10:41 -0400

    Made changes to how categories are added

--- src/code/Class.java
+++ src/code/Class.java
@@ -1,19 +1,36 @@
 package src.code;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import javafx.util.Pair;
 
 public class Class{
 
 	private String _className;
 	private ArrayList<Parts> _parts;
+	private HashMap<String, Double> _categories;
 
 	public Class(String className){
 		_className = className; 
 		_parts = new ArrayList<Parts>();
 	}
 
-	public void add(String part, double grade, double weight){
-		_parts.add(new Parts(part, grade, weight));
+    /**
+     *
+     * @param partName - Category name
+     * @param weight   - The weight of the category
+     * @return true if category does not exist 
+     */
+	public boolean addCategory (String partName, Double weight){
+	    if(_categories.containsKey(partName)){
+	        return false;
+        }
+        _categories.put(partName,weight);
+	    return true;
+    }
+
+	public void add(String part, double grade){
+		_parts.add(new Parts(part, grade));
 	}
 
 	public String getName(){
--- src/code/Parts.java
+++ src/code/Parts.java
@@ -1,36 +1,15 @@
 
 package src.code;
-/*
-public abstract class Parts{
-
-	private double _grade;
-
-	public Parts(double grade){
-
-		_grade = grade;
-	}
-
-	public double returnGrade(){
-
-	    return _grade;
-	}
-
-	public void setGrade(double grade){
-
-	    _grade = grade;
-
-	}
-}*/
 
 public class Parts{
 	private String _part;
 	private double _grade;
 	private double _percentage;
 
-	public Parts(String part, double grade, double percentage){
+	public Parts(String part, double grade){
 		_part = part;
 		_grade = grade;
-		_percentage = percentage;
+//		_percentage = percentage;
 	}
 }
 
